93e43a20699825dd3fd5b661ca2307a2fdf1e24f
Adarsh <maxmax.mishra@gmail.com>
2026-03-07 22:55:42 +0000

    exit implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <string>
 
+std::string trim(const std::string& s) {
+    size_t l = s.find_first_not_of(" \t\n\r");
+    if (l == std::string::npos) return "";
+    size_t r = s.find_last_not_of(" \t\n\r");
+    return s.substr(l, r - l + 1);
+}
+
 int main() {
-  // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
+
   while(true){
-  std::cout << "$ ";
-  std::string command;
-  std::getline(std::cin, command);
-  std::cout<< command <<": " <<"command not found" <<"\n";
+      std::cout << "$ ";
+      std::string command;
+      std::getline(std::cin, command);
+
+      if(trim(command) == "exit") break;
+
+      std::cout << command << ": command not found\n";
   }
 }
