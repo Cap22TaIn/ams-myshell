34ba917a9aec11cad4902c7972ae579d912dd0b3
Adarsh <maxmax.mishra@gmail.com>
2026-03-08 00:18:35 +0000

    cd bug solved

--- src/main.cpp
+++ src/main.cpp
@@ -24,7 +24,7 @@ int main() {
        std::string dir;
        ss >> dir;
        if(dir == "~") chdir(getenv("HOME"));
-       else if(access(dir.c_str(), F_OK)) chdir(dir.c_str());
+       else if(access(dir.c_str(), F_OK) == 0) chdir(dir.c_str());
        else std::cout << "cd:" << dir << ": No such file or directory" << "\n";
     }
     else if (command == "echo") {
