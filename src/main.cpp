6ad437e4d62884fbda400bc681ae6f7f37d379ce
Adarsh <maxmax.mishra@gmail.com>
2026-03-09 02:54:10 +0000

    stderr implementation

--- src/main.cpp
+++ src/main.cpp
@@ -12,95 +12,103 @@
 
 using namespace std;
 
-string find_command(const string& cmd) {
+string find_command(const string& cmd)
+{
     const char* path_env = getenv("PATH");
     if(!path_env) return "";
 
     stringstream ss(path_env);
     string dir;
 
-    while(getline(ss, dir, ':')) {
+    while(getline(ss,dir,':'))
+    {
         string full = dir + "/" + cmd;
-        if(access(full.c_str(), X_OK) == 0)
+
+        if(access(full.c_str(),X_OK)==0)
             return full;
     }
 
     return "";
 }
 
-int main() {
-    cout << unitbuf;
-    cerr << unitbuf;
+int main()
+{
+    cout<<unitbuf;
+    cerr<<unitbuf;
 
-    unordered_set<string> builtin = {"cd","echo","exit","pwd","type"};
+    unordered_set<string> builtin={"cd","echo","exit","pwd","type"};
+
     string line;
 
-    while(true) {
+    while(true)
+    {
+        cout<<"$ ";
 
-        cout << "$ ";
-        getline(cin, line);
+        getline(cin,line);
 
-        auto args = parse_command(line);
-        if(args.empty()) continue;
+        auto args=parse_command(line);
+
+        if(args.empty())
+            continue;
 
         string outfile;
+        string errfile;
+
         vector<string> cmd_args;
 
-        // Detect redirection
-        for(size_t i=0;i<args.size();i++) {
-
-            if(args[i] == ">" || args[i] == "1>") {
-                if(i+1 < args.size())
-                    outfile = args[i+1];
-                break;
+        for(size_t i=0;i<args.size();i++)
+        {
+            if(args[i]=="?>" || args[i]=="1>")
+            {
+                outfile=args[i+1];
+                i++;
             }
-
-            cmd_args.push_back(args[i]);
+            else if(args[i]==">")
+            {
+                outfile=args[i+1];
+                i++;
+            }
+            else if(args[i]=="2>")
+            {
+                errfile=args[i+1];
+                i++;
+            }
+            else
+            {
+                cmd_args.push_back(args[i]);
+            }
         }
 
-        string cmd = cmd_args[0];
+        if(cmd_args.empty())
+            continue;
 
-        // EXIT
-        if(cmd == "exit") break;
+        string cmd=cmd_args[0];
 
-        // ECHO
-        if(cmd == "echo") {
+        if(cmd=="exit")
+            break;
 
-            if(outfile.empty()) {
-                for(size_t i=1;i<cmd_args.size();i++){
-                    if(i>1) cout<<" ";
-                    cout<<cmd_args[i];
-                }
-                cout<<"\n";
-            }
+        if(cmd=="cd")
+        {
+            string dir = cmd_args.size()>1 ? cmd_args[1] : "";
 
-            else {
-                int fd = open(outfile.c_str(),
-                              O_WRONLY | O_CREAT | O_TRUNC,
-                              0644);
+            if(dir=="~")
+                dir=getenv("HOME");
 
-                for(size_t i=1;i<cmd_args.size();i++){
-                    if(i>1) dprintf(fd," ");
-                    dprintf(fd,"%s",cmd_args[i].c_str());
-                }
-
-                dprintf(fd,"\n");
-                close(fd);
-            }
+            if(chdir(dir.c_str())!=0)
+                cout<<"cd: "<<dir<<": No such file or directory\n";
 
             continue;
         }
 
-        // PWD
-        if(cmd == "pwd") {
-
+        if(cmd=="pwd")
+        {
             char buf[1024];
             getcwd(buf,sizeof(buf));
 
             if(outfile.empty())
                 cout<<buf<<"\n";
-
-            else{
+            else
+            {
                 int fd=open(outfile.c_str(),
                             O_WRONLY|O_CREAT|O_TRUNC,
                             0644);
@@ -112,36 +120,54 @@ int main() {
             continue;
         }
 
-        // CD
-        if(cmd == "cd") {
+        if(cmd=="echo")
+        {
+            if(outfile.empty())
+            {
+                for(size_t i=1;i<cmd_args.size();i++)
+                {
+                    if(i>1) cout<<" ";
+                    cout<<cmd_args[i];
+                }
 
-            string dir = cmd_args.size()>1 ? cmd_args[1] : "";
+                cout<<"\n";
+            }
+            else
+            {
+                int fd=open(outfile.c_str(),
+                            O_WRONLY|O_CREAT|O_TRUNC,
+                            0644);
 
-            if(dir=="~") dir=getenv("HOME");
+                for(size_t i=1;i<cmd_args.size();i++)
+                {
+                    if(i>1) dprintf(fd," ");
+                    dprintf(fd,"%s",cmd_args[i].c_str());
+                }
 
-            if(chdir(dir.c_str())!=0)
-                cout<<"cd: "<<dir<<": No such file or directory\n";
+                dprintf(fd,"\n");
+
+                close(fd);
+            }
 
             continue;
         }
 
-        // TYPE
-        if(cmd == "type") {
-
-            if(cmd_args.size()<2) continue;
+        if(cmd=="type")
+        {
+            if(cmd_args.size()<2)
+                continue;
 
             string target=cmd_args[1];
 
             if(builtin.count(target))
                 cout<<target<<" is a shell builtin\n";
 
-            else{
-
+            else
+            {
                 string path=find_command(target);
 
                 if(path.empty())
                     cout<<target<<": not found\n";
-
                 else
                     cout<<target<<" is "<<path<<"\n";
             }
@@ -149,21 +175,20 @@ int main() {
             continue;
         }
 
-        // EXTERNAL COMMAND
-
         string path=find_command(cmd);
 
-        if(path.empty()){
+        if(path.empty())
+        {
             cout<<cmd<<": command not found\n";
             continue;
         }
 
         pid_t pid=fork();
 
-        if(pid==0){
-
-            if(!outfile.empty()){
-
+        if(pid==0)
+        {
+            if(!outfile.empty())
+            {
                 int fd=open(outfile.c_str(),
                             O_WRONLY|O_CREAT|O_TRUNC,
                             0644);
@@ -172,9 +197,19 @@ int main() {
                 close(fd);
             }
 
+            if(!errfile.empty())
+            {
+                int fd=open(errfile.c_str(),
+                            O_WRONLY|O_CREAT|O_TRUNC,
+                            0644);
+
+                dup2(fd,STDERR_FILENO);
+                close(fd);
+            }
+
             vector<char*> argv;
 
-            for(auto& s:cmd_args)
+            for(auto &s:cmd_args)
                 argv.push_back(const_cast<char*>(s.c_str()));
 
             argv.push_back(nullptr);
@@ -183,10 +218,12 @@ int main() {
 
             exit(1);
         }
-
-        else{
+        else
+        {
             int status;
             waitpid(pid,&status,0);
         }
     }
+
+    return 0;
 }
